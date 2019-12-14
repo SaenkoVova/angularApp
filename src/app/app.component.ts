fb186851ec0553441c7f44050c42f91bbf40afff
SaenkoVova <vova080520@gmail.com>
2019-12-15 01:06:51 +0200

    trainning

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
 
+export interface Todo {
+  id: number
+  title: string
+  completed: boolean
+  date?: any
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +14,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularApp';
+
+  public todos:Todo[] = [
+    {id: 1, title: '12345', completed: false, date: new Date()},
+    {id: 2, title: '11111', completed: true, date: new Date()},
+    {id: 3, title: '22222', completed: false, date: new Date()}
+  ]
 }
---
+++ src/app/todos/todos.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Todo } from '../app.component';
+
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.css']
+})
+export class TodosComponent implements OnInit {
+
+  @Input() todos: Todo[] = []
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
