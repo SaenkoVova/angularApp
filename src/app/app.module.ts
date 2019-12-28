1a07a20286878efd068ff8989d0be2ad217654f2
SaenkoVova <vova080520@gmail.com>
2019-12-28 02:32:44 +0200

    Form module added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,14 @@ import { RouterModule } from '@angular/router'
 
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule  } from '@angular/forms';
 import { HeaderComponent } from './header/header.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { CartComponent } from './cart/cart.component';
-import { ShippingComponent } from './shipping/shipping.component'
+import { ShippingComponent } from './shipping/shipping.component';
+import { ToOrderComponent } from './to-order/to-order.component'
 
 @NgModule({
   declarations: [
@@ -20,17 +21,20 @@ import { ShippingComponent } from './shipping/shipping.component'
     ProductDetailsComponent,
     ProductAlertsComponent,
     CartComponent,
-    ShippingComponent
+    ShippingComponent,
+    ToOrderComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     FormsModule,
+    ReactiveFormsModule ,
     RouterModule.forRoot([
       {path: '', component: ProductListComponent},
       {path: 'products/:productId', component: ProductDetailsComponent},
       {path: 'cart', component: CartComponent},
-      {path: 'shipping', component: ShippingComponent}
+      {path: 'shipping', component: ShippingComponent},
+      {path: 'order', component: ToOrderComponent}
     ])
   ],
   providers: [],
---
+++ src/app/to-order/to-order.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+
+@Component({
+  selector: 'app-to-order',
+  templateUrl: './to-order.component.html',
+  styleUrls: ['./to-order.component.css']
+})
+export class ToOrderComponent implements OnInit {
+
+  checkoutForm
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) { 
+    this.checkoutForm = this.formBuilder.group({
+      name: '',
+      email: ''
+    })
+  }
+
+  ngOnInit() {
+  }
+
+  order(customerData) {
+    console.log(customerData)
+  }
+}
